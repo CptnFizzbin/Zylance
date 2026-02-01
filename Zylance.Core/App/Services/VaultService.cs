2bed9b3c8bf98582c42d6ce19a31d2787b909f60
CptnFizzbin <code@cptnfizzbin.ca>
2026-01-31 20:55:11 -0500

    refactor: Change VaultService to support only one active vault

--- Zylance.Core/App/Services/VaultService.cs
+++ Zylance.Core/App/Services/VaultService.cs
@@ -1,17 +1,25 @@
 using Zylance.Contract.Models.Vault;
-using Zylance.Core.Lib.Interfaces;
+using Zylance.Core.Lib.Vault;
 
 namespace Zylance.Core.App.Services;
 
+public class VaultException(string message) : Exception(message)
+{
+    public static VaultException NoActiveVault()
+    {
+        return new VaultException("No active vault. Please open or create a vault before performing operations.");
+    }
+}
+
 public class VaultService(IVaultProvider vaultProvider)
 {
-    private readonly Dictionary<string, IVault> _openedVaults = new();
+    public IVault? ActiveVault { get; private set; }
 
     public VaultRef OpenVault()
     {
         var vault = vaultProvider.OpenVault();
         var vaultId = Guid.NewGuid().ToString();
-        _openedVaults[vaultId] = vault;
+        ActiveVault = vault;
         return new VaultRef { Id = vaultId };
     }
 
@@ -19,14 +27,7 @@ public class VaultService(IVaultProvider vaultProvider)
     {
         var vault = vaultProvider.CreateVault();
         var vaultId = Guid.NewGuid().ToString();
-        _openedVaults[vaultId] = vault;
+        ActiveVault = vault;
         return new VaultRef { Id = vaultId };
-    }
-
-    public IVault GetVault(VaultRef vaultRef)
-    {
-        return _openedVaults.TryGetValue(vaultRef.Id, out var vault)
-            ? vault
-            : throw new KeyNotFoundException($"Vault with ID '{vaultRef.Id}' is not open");
     }
 }
